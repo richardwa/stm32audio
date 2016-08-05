2b6a9adb989841533190825e5544ac60d4d23d93
rich <me@home>
2016-08-05 00:19:22 -0400

    working with 2 note polyphony

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # stm32audio
 audio experiments on stm32
+  serial upload instructions for stm32f1 maple mini clone: 
+  ftdi rx to PA9
+  ftdi tx to PA10 
+  ftdi vcc to 3.3v
+  ftdi ground to ground
+  
+  ftdi set to 3.3V
+  Boot0 at 1 position (top jumper when usb socket is to your left)
+
+  sketch will run after upload, but not on power on if boot0 is at 1
+
+  press reset if sketch is running to place board back to upload mode
--- stm32audio.ino
+++ stm32audio.ino
@@ -1,69 +1,102 @@
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
-
-  serial upload instructions for stm32f1 maple mini clone: 
-  ftdi rx to PA9
-  ftdi tx to PA10 
-  ftdi vcc to 3.3v
-  ftdi ground to ground
-  
-  ftdi set to 3.3V
-  Boot0 at 1 position (top jumper when usb socket is to your left)
-
-  sketch will run after upload, but not on power on if boot0 is at 1
-
-  press reset if sketch is running to place board back to upload mode
-  
- */
 
 #define led PC13
 
+//playing at 22 micros period, we need the buffer to cover at least 0.05 seconds for the low notes
+//highest pitch alternating +/- which comes to 20khz
+//lowest pitch is all one period in 0.05
+#define bufferLength 2000
+#define bufferHeight 256
+#define sampleRate 22000
+int8_t playbuffer[bufferLength] = {0}; //contains negatives
+volatile uint16_t bufferPos = 0;
+void play() {
+  uint8_t value = playbuffer[bufferPos] + bufferHeight / 2; //convert to unsigned
+  uint8_t notval = ~value;
+  GPIOA->regs->BSRR = value | (notval << 16);
+  bufferPos = (bufferPos + 1) % bufferLength;
+}
+
+void printBuffer() {
+  for (uint16_t i = 0; i < bufferLength; i++) {
+    Serial.print(playbuffer[i]+bufferHeight / 2,HEX);
+    Serial.print(" ");
+  }
+  Serial.println();
+}
 
 // the setup function runs once when you press reset or power the board
 void setup() {
+  Serial.begin(115200);
+  printBuffer();
+  sawtooth(440);
+  sawtooth(660);
+  printBuffer();
+
   // initialize digital pin 13 as an output.
   pinMode(led, OUTPUT);
-
   //set this ground for reduced wiring =)
-  pinMode(PC15,OUTPUT);
-  digitalWrite(PC15,0);
-  
-  pinMode(PA0,OUTPUT);
-  pinMode(PA1,OUTPUT);
-  pinMode(PA2,OUTPUT);
-  pinMode(PA3,OUTPUT);
-  pinMode(PA4,OUTPUT);
-  pinMode(PA5,OUTPUT);
-  pinMode(PA6,OUTPUT);
-  pinMode(PA7,OUTPUT);
-  
-  Serial.begin(115200);
-}
-volatile uint8_t count = 0;
+  pinMode(PC15, OUTPUT);
+  digitalWrite(PC15, 0);
 
-void dacOutput(uint8_t value){
-  uint8_t notval = ~value;
-  GPIOA->regs->BSRR = value | (notval << 16);
-//  digitalWrite(PA0,value & (1<<7));
-//  digitalWrite(PA1,value & (1<<6));
-//  digitalWrite(PA2,value & (1<<5));
-//  digitalWrite(PA3,value & (1<<4));
-//  digitalWrite(PA4,value & (1<<3));
-//  digitalWrite(PA5,value & (1<<2));
-//  digitalWrite(PA6,value & (1<<1));
-//  digitalWrite(PA7,value & (1<<0));
+  //set our R2R ladder DAC to output
+  pinMode(PA0, OUTPUT);
+  pinMode(PA1, OUTPUT);
+  pinMode(PA2, OUTPUT);
+  pinMode(PA3, OUTPUT);
+  pinMode(PA4, OUTPUT);
+  pinMode(PA5, OUTPUT);
+  pinMode(PA6, OUTPUT);
+  pinMode(PA7, OUTPUT);
+
+  //configure the sample timer
+  HardwareTimer sampleTimer(1);
+  sampleTimer.pause();
+
+  sampleTimer.setPeriod(1000000 / sampleRate);
+  sampleTimer.setMode(TIMER_CH1, TIMER_OUTPUT_COMPARE);
+  sampleTimer.setCompare(TIMER_CH1, 1);
+  sampleTimer.attachInterrupt(TIMER_CH1, play);
+
+  sampleTimer.refresh();
+  sampleTimer.resume();
+
 }
 
-//outputs 0-255 at 256 x positions
-uint8_t taylorCos(uint8_t x) {
-  //taylor series approx: 1 - x^2/2!  + x^4/4!
-   return 1 - (x*x)/2 ;
+
+//set a sawtooth wave into buffer;
+void sawtooth(uint16_t freq) {
+  uint16_t period = sampleRate / freq;
+  for (uint16_t i = 0; i < bufferLength; i++) {
+    playbuffer[i] += ((i % period) * bufferHeight / period - bufferHeight / 2)/2;
+  }
 }
 
-// the loop function runs over and over again forever
+//taylor series cos approx: 1 - x^2/2!  + x^4/4!
+#define inputLength 10
+char inputBuffer[inputLength];
+uint8_t inputIndex = 0;
+boolean inputDone = false;
 void loop() {
-  dacOutput(count);   
-  count++;
-  delayMicroseconds(10);
+  // send data only when you receive data:
+  if (Serial.available() > 0) {
+    char in = Serial.read();
+    inputBuffer[inputIndex] = in;
+    if (in == '\n' || inputIndex == inputLength - 1) {
+      inputDone = true;
+    } else {
+      inputIndex++;
+    }
+  }
+  
+  if (inputDone) {
+    Serial.write(inputBuffer, inputIndex);
+    Serial.println();
+    uint16_t value = atol(inputBuffer);
+    sawtooth(value);
+    printBuffer();
+    inputIndex = 0;
+    inputDone = false;
+  }
 }
+
+
