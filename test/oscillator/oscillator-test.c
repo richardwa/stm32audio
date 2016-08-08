303eeaba690b67d92af3bb4876cd5e4a94144231
rich <me@home>
2016-08-08 02:02:12 -0400

    sine working at 440

--- test/oscillator/oscillator-test.c
+++ test/oscillator/oscillator-test.c
@@ -4,15 +4,16 @@
 #include "oscillator.h"
 
 
-#define period 20
+#define period  100
+#define samples (2*period)
 int main()
 {
   int i;
-  uint16_t playbuffer;
-  for (i = 0; i < 100; i++) {
+  for (i = 0; i < samples; i++) {
 //    printf("%d,", sawtooth(500,i));
-    printf("%d,", triangle(period,i));
+//    printf("%d,", triangle(period,i));
 //    printf("%d,", square(period,i));
+    printf("%d,", sine(period,i));
     printf("\n");
   }
   return 0;
