8f64ba6e49c369e689c84935195766f98eb90cdc
rich <me@home>
2016-08-09 01:36:19 -0400

    sine approx is good

--- stm32audio/oscillator.c
+++ stm32audio/oscillator.c
@@ -1,12 +1,12 @@
 #include "oscillator.h"
 #include "debug.h"
 
-#define LEVEL 1000
+#define LEVEL 10000
 
 //since our sine cubic coeffcients are floats, we need to incorporate LEVEL into it
-#define CUBIC_A -429 // -0.4292036732
-#define CUBIC_B -141 // -0.1415926536
-#define CUBIC_C 1570 //  1.570796327
+#define SIN_CUBIC_A -429 // -0.4292036732
+#define SIN_CUBIC_B -141 // -0.1415926536
+#define SIN_CUBIC_C 1570 //  1.570796327
 
 int32_t sawtooth(int32_t period, uint32_t phase)
 {
@@ -36,8 +36,13 @@ int32_t square(int32_t period, uint32_t phase)
 
 //cubic aprroximation of sine
 // base function: f(x) = ax^3 + bx^2 + cx
-// solve for desired sine conditions, we get:
+//               f'(x) = 3ax^2 + 2bx + c
+//
+// desired conditions, matching sin(pi/2 *x):
+// f(0) = 0, f(1) = 1, f'(0) = pi/2 , f'(1) = 0
+// solve the system we get:
 // a = pi/2 - 2, b = -pi + 3, c = pi/2
+//
 // a ~ -0.4292036732
 // b ~ -0.1415926536
 // c ~ 1.570796327
@@ -48,24 +53,32 @@ int32_t square(int32_t period, uint32_t phase)
 int32_t sine(int32_t period, uint32_t phase)
 {
 
-  float x = phase % period;
+  int32_t x = phase % period;
   uint8_t section = x * 4 / period;
 
   switch(section) {
     case 0:  //the is the fist quarter of the the wave
-      return (   CUBIC_A * 4*4*4 *x/period*x/period + CUBIC_B * 4*4 *x/period +  CUBIC_C * 4)*x/period;
+      return SIN_CUBIC_A * 4*4*4 *x/period*x/period*x/period 
+           + SIN_CUBIC_B * 4*4 *x/period*x/period 
+           + SIN_CUBIC_C * 4 *x/period;
     case 1:  
       x =  period / 2 - x; // mirror flip into first quater
-      return (   CUBIC_A * 4*4*4 *x/period*x/period + CUBIC_B * 4*4 *x/period +  CUBIC_C * 4)*x/period;
+      return SIN_CUBIC_A * 4*4*4 *x/period*x/period*x/period 
+           + SIN_CUBIC_B * 4*4 *x/period*x/period 
+           + SIN_CUBIC_C * 4 *x/period;
     case 2:  
       x =  x - period / 2; // translate to first quater
       //negative of first half
-      return (-1*CUBIC_A * 4*4*4 *x/period*x/period - CUBIC_B * 4*4 *x/period -  CUBIC_C * 4)*x/period;
+      return -1*SIN_CUBIC_A * 4*4*4 *x/period*x/period*x/period 
+              - SIN_CUBIC_B * 4*4 *x/period*x/period 
+              - SIN_CUBIC_C * 4 *x/period;
     case 3:
       x =  x - period / 2; // translate to second quater
       x =  period / 2 - x; // mirror flip into first quater
       //negative of first half
-      return (-1*CUBIC_A * 4*4*4 *x/period*x/period - CUBIC_B * 4*4 *x/period -  CUBIC_C * 4)*x/period;
+      return -1*SIN_CUBIC_A * 4*4*4 *x/period*x/period*x/period 
+              - SIN_CUBIC_B * 4*4 *x/period*x/period 
+              - SIN_CUBIC_C * 4 *x/period;
       
   };
 }
